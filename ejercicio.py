be19bd5c7aef36415349f0ff3829df9b165e66aa
MarianaOsorioHernandez <146780426+MarianaOsorioHernandez@users.noreply.github.com>
2025-09-27 16:53:19 -0500

    Update ejercicio.py
    add multiplify operation

--- ejercicio.py
+++ ejercicio.py
@@ -9,3 +9,5 @@ print(f"El resultado de la resta entre {a} y {b} es: {d}")
 e = a * b
 print(f"El resultado de la multiplicación entre {a} y {b} es: {e}")
 
+e = a // b
+print(f"El resultado de la división entera de {a} entre {b} es: {e}")
