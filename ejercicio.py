2be81517cd2979c81f7a112d48576267ffb8e881
StevenCu07 <brandon.steven1259@hotmail.com>
2025-09-27 16:52:18 -0500

    subo multiplicacion

--- ejercicio.py
+++ ejercicio.py
@@ -6,3 +6,6 @@ print(f"El resultado de la suma de {a} y {b} es: {c}")
 d = a - b
 print(f"El resultado de la resta entre {a} y {b} es: {d}")
 
+e = a * b
+print(f"El resultado de la multiplicación entre {a} y {b} es: {e}")
+
